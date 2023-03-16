eb8e45453b1a189503dd6a5c16ef6cca6737b785
Łukasz Sarnacki <lukasz.sarnacki@gmail.com>
2023-03-16 14:15:02 +0100

    nvim-dev-container

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -1,83 +1,88 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use({ 'ellisonleao/gruvbox.nvim' })
+
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+  use('mbbill/undotree')
+
+  use('tpope/vim-fugitive')
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+
+      -- shows status of nvim-lsp progress
+      { 'j-hui/fidget.nvim' },
+
+      -- shows icons for cmp
+      { 'onsails/lspkind.nvim' },
+
+      -- somehow python format doesn't work without it
+      { 'jose-elias-alvarez/null-ls.nvim' }
     }
+  }
 
-    use({ 'ellisonleao/gruvbox.nvim' })
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  }
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'preservim/nerdtree'
 
-    use('mbbill/undotree')
-
-    use('tpope/vim-fugitive')
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-
-            -- shows status of nvim-lsp progress
-            { 'j-hui/fidget.nvim' },
-
-            -- shows icons for cmp
-            { 'onsails/lspkind.nvim' },
-
-            -- somehow python format doesn't work without it
-            { 'jose-elias-alvarez/null-ls.nvim' }
-        }
+  use {
+    "nvim-neotest/neotest",
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "jfpedroza/neotest-elixir",
+      "nvim-neotest/neotest-python",
     }
+  }
 
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup {}
-        end
-    }
+  use {
+    'simrat39/symbols-outline.nvim',
+    config = function() require("symbols-outline").setup() end
+  }
 
-    use 'preservim/nerdtree'
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
 
-    use {
-        "nvim-neotest/neotest",
-        requires = {
-            "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim",
-            "jfpedroza/neotest-elixir",
-            "nvim-neotest/neotest-python",
-        }
-    }
-
-    use {
-        'simrat39/symbols-outline.nvim',
-        config = function() require("symbols-outline").setup() end
-    }
-
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+  use {
+    'https://codeberg.org/esensar/nvim-dev-container',
+    requires = { 'nvim-treesitter/nvim-treesitter' }
+  }
 end)
