02242321225ee17170c82d5324cbe933395d975f
Łukasz Sarnacki <lukasz.sarnacki@printbear.net>
2024-02-01 15:42:42 +0100

    Disable mini.indentscope

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
   -- disable trouble
   { "folke/flash.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false }
+  { "rcarriga/nvim-notify", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false}
 }
