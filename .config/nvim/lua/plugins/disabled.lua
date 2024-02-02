ab2c5cf56a137daf19414daed2fa2bccfa31f396
Łukasz Sarnacki <lukasz.sarnacki@printbear.net>
2024-02-02 10:34:49 +0100

    disable more plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,8 +1,11 @@
 return {
-  -- disable trouble
   { "folke/flash.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false},
-  { "lukas-reineke/indent-blankline.nvim", enabled = false}
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.ai", enabled = false }, -- worth looking into in the future
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
 }
