9d11eb2d6b1dd0c936bcb6ab7f4c5e3a47422a57
Łukasz Sarnacki <lukasz.sarnacki@printbear.net>
2025-05-27 09:13:52 +0200

    Prepare for embeding in VSC

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,22 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- -- bootstrap lazy.nvim, LazyVim and your plugins
+-- if vim.g.vscode then
+--   vim.opt.number = true -- Enable line numbers in VSCode Neovim
+--   vim.opt.relativenumber = true -- Disable relative numbers for simplicity
+--   vim.opt.cursorline = true -- Highlight the current line
+--   vim.opt.wrap = true
+-- else
 require("config.lazy")
+-- end
+--
+if vim.g.vscode then
+  vim.api.nvim_set_keymap("n", "<C-o>", "<C-o>", { noremap = true, silent = true })
+  -- Map <leader>e to open the file explorer in VSCode
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>e",
+    ':call VSCodeNotify("workbench.view.explorer")<CR>',
+    { noremap = true, silent = true }
+  )
+
+  vim.api.nvim_set_keymap("n", "<leader>r", ":source $MYVIMRC<CR>", { noremap = true, silent = true })
+end
